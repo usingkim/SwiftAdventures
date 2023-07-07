03d18d0109eba79f7e397bfe691108c9c83d47c0
usingkim <kimyujin173@gmail.com>
2023-07-07 11:35:36 +0900

    View Grouping

---
+++ Stickers/Stickers/Model/Sticker.swift
@@ -0,0 +1,16 @@
+//
+//  Sticker.swift
+//  Stickers
+//
+//  Created by 김유진 on 2023/07/07.
+//
+
+import Foundation
+import SwiftUI
+
+// memo 내용, color, date
+struct Sticker {
+    var memo: String
+    var color: Color
+    var date: Date
+}
---
+++ Stickers/Stickers/Model/StickerStore.swift
@@ -0,0 +1,8 @@
+//
+//  StickerStore.swift
+//  Stickers
+//
+//  Created by 김유진 on 2023/07/07.
+//
+
+import Foundation
