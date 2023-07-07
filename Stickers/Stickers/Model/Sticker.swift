442145bf8b655e34f66f2eb693745b4909d5a9cb
usingkim <kimyujin173@gmail.com>
2023-07-07 13:11:15 +0900

    make Models

--- Stickers/Stickers/Model/Sticker.swift
+++ Stickers/Stickers/Model/Sticker.swift
@@ -9,8 +9,16 @@ import Foundation
 import SwiftUI
 
 // memo 내용, color, date
-struct Sticker {
+struct Sticker : Identifiable {
+    // id value 굳이 만들어주는 이유? Identifier, List
+    var id: UUID = UUID()
     var memo: String
     var color: Color
     var date: Date
+    
+    var dateString: String {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        return dateFormatter.string(from: date)
+    }
 }
--- Stickers/Stickers/Model/StickerStore.swift
+++ Stickers/Stickers/Model/StickerStore.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+// 그릇
+class StickerStore: ObservableObject {
+    var stickers: [Sticker] = []
+    
+    init() {
+        stickers = [
+            Sticker(memo: "Hello", color: .blue, date: Date()),
+            Sticker(memo: "Hi", color: .cyan, date: Date()),
+            Sticker(memo: "Good Afternoon!", color: .brown, date: Date())
+        ]
+    }
+}
