a3b51eb955b83bad740462d3d2e9c4670b2d04a9
usingkim <kimyujin173@gmail.com>
2023-07-07 09:15:35 +0900

    make Stickers project

---
+++ Stickers/Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by 김유진 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
