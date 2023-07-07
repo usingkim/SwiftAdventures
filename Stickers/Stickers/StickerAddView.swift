b7711055b3eb0a27b71f86f5bb1e50864c9be5e4
usingkim <kimyujin173@gmail.com>
2023-07-07 10:21:12 +0900

    ListView

---
+++ Stickers/Stickers/StickerAddView.swift
@@ -0,0 +1,20 @@
+//
+//  StickerAddView.swift
+//  Stickers
+//
+//  Created by 김유진 on 2023/07/07.
+//
+
+import SwiftUI
+
+struct StickerAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StickerAddView_Previews: PreviewProvider {
+    static var previews: some View {
+        StickerAddView()
+    }
+}
--- Stickers/Stickers/StickerListView.swift
+++ Stickers/Stickers/StickerListView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 
 struct StickerListView: View {
+    // Binding으로 넘기는 Bool, Int 같은 기본값에는 State Property Wrapper를 꼭 달아줘야한다.
+    @State var isSheetPresented: Bool = false
+    
     var body: some View {
         VStack{
             List{
@@ -20,13 +23,28 @@ struct StickerListView: View {
                 StickerView()
             }
             
-//            Label("Add", systemImage: "plus.app")
-            Image(systemName: "plus.app")
-                .font(.largeTitle)
-                .padding()
+            //            Label("Add", systemImage: "plus.app")
+            //            Image(systemName: "plus.app")
+            //                .font(.largeTitle)
+            //                .padding()
         }
         .listStyle(.plain)
         .navigationTitle("Stickers")
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    isSheetPresented = true // toggle보다는 true/false로 명시적으로 적어주는게 좋을지도..
+                } label: {
+                    Label("Add", systemImage: "pencil.tip.crop.circle.badge.plus")
+                }
+
+                
+            }
+        }
+        // content에는 View 이름
+        .sheet(isPresented: $isSheetPresented) {
+            StickerAddView()
+        }
         
     }
 }
